d18eade11f192c089070ee7cb0e5e55bd4aad6db
harshad224 <pawarharshad224@gmail.com>
2026-03-01 17:42:11 +0530

    api index file chanegd

--- api/index.js
+++ api/index.js
@@ -1,26 +1,23 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const userRoute=require("./routes/sendMailRoute")
-const cors=require("cors")
+const userRoute = require("./routes/sendMailRoute");
+const cors = require("cors");
 
-
-// Load environment variables
 dotenv.config();
+
 const app = express();
+
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
     methods: ["GET", "POST"],
     credentials: true,
   })
-)
-app.use(express.json());
-// Define a basic route
-app.use('/',userRoute);
-// Start the server
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//    console.log(`Server running on http://localhost:${PORT}`);
-// });
+);
 
-export default app;
+app.use(express.json());
+
+// Mount routes (correct)
+app.use(userRoute);
+
+module.exports = app;
